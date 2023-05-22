a0b1a8e41996d6e28f9659444d76808eda23fc07
Gabrielrz <byron.rz@hotmail.com>
2023-05-22 13:52:12 +0200

    config build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: 'Gabrielrz/portfolio-gabriel/', // Reemplaza 'nombre-repositorio' con el nombre de tu repositorio en GitHub
+  base: '', // Reemplaza 'nombre-repositorio' con el nombre de tu repositorio en GitHub
 })
