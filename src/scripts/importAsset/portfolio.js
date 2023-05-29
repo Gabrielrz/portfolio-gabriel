589e5c741ef2cff5d77aeac705472cd0edbeb337
Gabrielrz <byron.rz@hotmail.com>
2023-05-29 23:35:17 +0200

    anadiendo enlaces

--- src/scripts/importAsset/portfolio.js
+++ src/scripts/importAsset/portfolio.js
@@ -1,10 +1,12 @@
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+import LodL from '../performance/levelOfDetail';
 export default class PortfolioGTLF{
 
     constructor(scene,url){
-			this.lod = new THREE.LOD();
+			
+			this.lodL = new LodL();
 			this.loadingManager = new THREE.LoadingManager()
 			this.url = url;
 			this.scene = scene;
@@ -27,8 +29,8 @@ export default class PortfolioGTLF{
 			this.loader.load(this.url, (gltf) => {
 
 				this.model =  gltf.scene;
-				//this.initLod(gltf,this.scene);
-				this.scene.add(this.model);
+				this.lodL.initLod(gltf,this.scene);
+				//this.scene.add(this.model);
 				this.position.copy(this.model.position);
 				this.camera = gltf.cameras[0];
 
@@ -40,7 +42,6 @@ export default class PortfolioGTLF{
 				//this.clip = THREE.AnimationClip.findByName( gltf.animations, "CameraAction" ) 
 
 				resolve(gltf);
-				console.log(gltf.scene);
 		
 
 			});
@@ -50,41 +51,5 @@ export default class PortfolioGTLF{
 		this.mixer = mixer;
 	}
 
-	update() {
-		//this.position.copy(this.model.position);
-	  }
-
-
-	initLod(gltf,scene){
-		// Distancia en la que se activa el nivel de detalle 
-		const distanceMedio = 10; 
-		const distanceBajo = 20; 
-
-		
-
-		// Nivel de detalle más alto (alta calidad)
-		const highQualityModel = gltf.scene.clone();
-		this.lod.addLevel(highQualityModel, 0);
-
-		// Nivel de detalle medio (calidad media)
-		const mediumQualityModel = highQualityModel.clone();
-		mediumQualityModel.traverse((child) => {
-		if (child.isMesh) {
-			// Ajusta los materiales, geometrías, etc., para reducir la calidad
-		}
-		});
-		this.lod.addLevel(mediumQualityModel, distanceMedio);
-
-		// Nivel de detalle más bajo (baja calidad)
-		const lowQualityModel = highQualityModel.clone();
-		lowQualityModel.traverse((child) => {
-		if (child.isMesh) {
-			// Ajusta los materiales, geometrías, etc., para reducir la calidad aún más
-		}
-		});
-		this.lod.addLevel(lowQualityModel, distanceBajo);
-
-		scene.add(this.lod);
-	}
 
 }
---
+++ src/scripts/performance/levelOfDetail.js
@@ -0,0 +1,60 @@
+import * as THREE from 'three';
+import EventEmitter from "../utils/eventEmitter";
+
+export default class LodL extends EventEmitter{
+    constructor(){
+        super();
+        this.lod = new THREE.LOD();
+        
+        this.levels = {0:0,1:30,2:50};
+        this.objects = new Array();
+        this.currentLevel =  this.lod.getCurrentLevel();
+    }
+    
+    initLod(gltf,scene){
+	
+		
+		
+
+		// Nivel de detalle más alto (alta calidad)
+		//const highQualityModel = gltf.scene.clone();
+        const highQualityModel = gltf.scene.clone();
+		this.lod.addLevel(highQualityModel, this.levels[0]);
+
+		// Nivel de detalle medio (calidad media)
+		const mediumQualityModel = highQualityModel.clone();
+		mediumQualityModel.traverse((child) => {
+		if (child.isMesh) {
+			// Ajusta los materiales, geometrías, etc., para reducir la calidad
+		}
+		});
+		this.lod.addLevel(mediumQualityModel, this.levels[1]);
+
+		// Nivel de detalle más bajo (baja calidad)
+		const lowQualityModel = highQualityModel.clone();
+		lowQualityModel.traverse((child) => {
+		if (child.isMesh) {
+			// Ajusta los materiales, geometrías, etc., para reducir la calidad aún más
+		}
+		});
+		this.lod.addLevel(lowQualityModel, this.levels[2]);
+
+		scene.add(this.lod);
+        
+        this.objects.push(highQualityModel);
+        this.objects.push(mediumQualityModel);
+        this.objects.push(lowQualityModel);
+       
+	}
+
+    
+    checkLODLevel(camera) {
+        const cameraDistance = camera.position.distanceTo(this.lod.position);
+        if(this.currentLevel!=this.lod.getCurrentLevel()){
+            this.currentLevel = this.lod.getCurrentLevel();
+            this.trigger('level_changed');
+            console.log(cameraDistance);
+            console.log(this.lod.getCurrentLevel());
+        }
+    }
+}
